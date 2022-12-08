e4d8abc0d44132cc33b57418e5fbe50542828401
Nguyen Trung Tin <102871319+nguyentrungtins@users.noreply.github.com>
2022-12-08 19:28:27 +0700

    Update install_terminal.sh

--- install_terminal.sh
+++ install_terminal.sh
@@ -4,3 +4,6 @@ set -eux pipefail
 # Install ZSH
 sudo apt install -y git-core zsh curl
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install Neofetch
+sudo apt install neofetch
